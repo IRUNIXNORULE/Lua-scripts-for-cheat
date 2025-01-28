ea0e7994ae4323f90dbd4e6f43b0a7b239c6e82b
KuRiGow <kansiisback@gmail.com>
2025-01-28 22:43:23 +0700

    Auto Cast + Auto Reel.lua

---
+++ SRC.lua
@@ -0,0 +1,39 @@
+-- Auto Cast
+local function autoCast()
+    local player = game:GetService("Players").LocalPlayer
+    local rod = nil
+    for _, item in pairs(player.Character:GetChildren()) do
+        if item:IsA("Tool") and item.Name:match("Rod") and item:FindFirstChild("events") and item.events:FindFirstChild("cast") then
+            rod = item
+            break
+        end
+    end
+---- Rod Found ----
+    if rod then
+        local args = {
+            [1] = 100,
+            [2] = 1
+        }
+        rod.events.cast:FireServer(unpack(args))
+    else
+        warn("No Rod Found!")
+    end
+end
+
+-- Auto Finish Reel
+local function autoReel()
+    local events = game:GetService("ReplicatedStorage"):FindFirstChild("events")
+    if events and events:FindFirstChild("reelfinished ") then
+        local args = {
+            [1] = 100,
+            [2] = true
+        }
+        events:FindFirstChild("reelfinished "):FireServer(unpack(args))
+    end
+end
+
+while task.wait(1) do
+    autoCast()
+    task.wait(0.5)
+    autoReel()
+end
